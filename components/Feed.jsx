8f9c8430acf64f9ecffcf511b5415b055d56d43c
Yaseen Nur <paradushex@gmail.com>
2022-06-15 12:12:00 +0600

    updated feed and videos component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,8 @@
 import { Box, Stack, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import Sidebar from "./Sidebar";
-import Videos from "./Videos";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
@@ -17,19 +17,20 @@ const Feed = () => {
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "2px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 Yaseen Nur
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex:2}}>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-        <Videos/>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
--- utils/fetchFromAPI.js
+++ utils/fetchFromAPI.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL=process.env.BASE_URL
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
   return data;
 };
