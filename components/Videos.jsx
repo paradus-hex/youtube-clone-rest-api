642eca239ff5685a7075ddbfc30d7bb9217b9bec
Yaseen Nur <paradushex@gmail.com>
2022-06-20 12:12:00 +0600

    added loader and videos component

--- components/Videos.jsx
+++ components/Videos.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import { Loader } from './';
 const Videos = ({videos}) => {
+  if(!videos?.length) return <Loader />;
   console.log(videos,"helloo")
   return (
-    <div>Videos</div>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
