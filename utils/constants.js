445b9dcba7b767127adb0cbb02bbf5c11d73a77b
Yaseen Nur <paradushex@gmail.com>
2022-06-24 12:12:00 +0600

    updated categories

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
+import { SportsSoccer } from '@mui/icons-material';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import CodeIcon from '@mui/icons-material/Code';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
@@ -6,7 +7,7 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import HomeIcon from '@mui/icons-material/Home';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import NightlightIcon from '@mui/icons-material/Nightlight';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SchoolIcon from '@mui/icons-material/School';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
@@ -17,20 +18,19 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
-  { name: 'NextJS', icon: <CodeIcon />, },
-  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Islam', icon: <NightlightIcon />, },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
   { name: 'Movie', icon: <OndemandVideoIcon />, },
   { name: 'Gaming', icon: <SportsEsportsIcon />, },
   { name: 'Live', icon: <LiveTvIcon />, },
-  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Sport', icon: <SportsSoccer />, },
   { name: 'Fashion', icon: <CheckroomIcon />, },
   { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
   { name: 'Comedy', icon: <TheaterComedyIcon />, },
-  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Fitness', icon: <FitnessCenterIcon />, },
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
+
 ];
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
