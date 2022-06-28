d2aa66b1bad2077f9bfad32e827bb65c62f08c4a
Yaseen Nur <paradushex@gmail.com>
2022-06-28 12:12:00 +0600

    channel details component

--- components/ChannelDetail.jsx
+++ components/ChannelDetail.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { ChannelCard, Videos } from "./";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+  const router=useRouter()
 
-export default ChannelDetail
+  const {id}=router.query
+
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
