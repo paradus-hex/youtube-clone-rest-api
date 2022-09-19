eaba667d29d9f256da09796381df050312305448
Yaseen Nur <paradushex@gmail.com>
2022-09-19 07:38:16 +0600

    added logo to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,17 @@
+import { Stack } from '@mui/system'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { logo } from "../utils/constants.js"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <Stack direction='row' alignItems='center' p={2} sx={{position:'sticky', background:'#000', top:0, justifyContent:'space-between'}}>
+    <Link href='/'>
+    <Image loader={()=>logo} src={logo} alt="logo" height={45} width={45}/>
+    </Link>
+   </Stack>
   )
 }
 
-export default Navbar
+export default Navbar 
