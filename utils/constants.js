91cbea9127e9b7585c0e24a2c09c8934105aae78
Yaseen Nur <paradushex@gmail.com>
2022-09-21 20:49:20 +0600

    updated constants

--- utils/constants.js
+++ utils/constants.js
@@ -33,9 +33,9 @@ export const categories = [
 
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
-export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
+export const demoThumbnailUrl = '/public/reinforz.ico';
+export const demoChannelUrl = '/channel/UC5qOPSFsVDRjnn5fyh2nADQ';
+export const demoVideoUrl = '/video/F9-daautlmk';
+export const demoChannelTitle = 'Reinforz';
+export const demoVideoTitle = 'Introducing Reinforz';
+export const demoProfilePicture = '/public/reinforz.ico'
